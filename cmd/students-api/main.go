0e8e57727f1344412d736256149ac0a7123b7cc7
thapaRoyal <thaparoyal17@gmail.com>
2025-05-19 00:13:53 +0545

    create student

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/royalthapa/students-api/internal/config"
 	"github.com/royalthapa/students-api/internal/http/handlers/student"
+	"github.com/royalthapa/students-api/internal/storage/sqlite"
 )
 
 func main() {
@@ -19,9 +20,16 @@ func main() {
 	cfg := config.MustLoad()
 	// database setup
 
+	storage, err := sqlite.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("storage initialized", slog.String("env", cfg.Env), slog.String("version", "1.0.0"))
+
 	// setup router
 	router := http.NewServeMux()
-	router.HandleFunc("POST /api/students", student.New())
+	router.HandleFunc("POST /api/students", student.New(storage))
 
 	// setup server
 	server := http.Server{
@@ -50,7 +58,7 @@ func main() {
 
 	defer cancel()
 
-	err := server.Shutdown(ctx)
+	err = server.Shutdown(ctx)
 
 	if err != nil {
 		slog.Error("Failed to shutdown server", slog.String("Error:", err.Error()))
--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -9,11 +9,12 @@ import (
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/royalthapa/students-api/internal/storage"
 	"github.com/royalthapa/students-api/internal/types"
 	"github.com/royalthapa/students-api/internal/utils/response"
 )
 
-func New() http.HandlerFunc {
+func New(storage storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		slog.Info("creating a student")
 
@@ -37,6 +38,15 @@ func New() http.HandlerFunc {
 			return
 		}
 
-		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "ok"})
+		lastId, err := storage.CreateStudent(student.Name, student.Email, student.Age)
+
+		slog.Info("user created successfully", slog.String("userId", fmt.Sprint(lastId)))
+
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		response.WriteJson(w, http.StatusCreated, map[string]int64{"id": lastId})
 	}
 }
