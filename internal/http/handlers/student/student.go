dcb00e81ceb05b01023016a5f14858f0ed69eed6
thapaRoyal <thaparoyal17@gmail.com>
2025-05-19 00:52:04 +0545

    get all students

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -75,3 +75,18 @@ func GetById(storage storage.Storage) http.HandlerFunc {
 		response.WriteJson(w, http.StatusOK, student)
 	}
 }
+
+func GetList(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("getting all students")
+
+		students, err := storage.GetStudents()
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, students)
+
+	}
+}
